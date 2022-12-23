4c9f6cce5f9439c859e4df489929255f332d4b78
lilingkang <872706089@qq.com>
2022-12-23 16:40:30 +0800

    Initial commit

---
+++ Source/ue2ros/ue2ros.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ue2ros.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ue2ros, "ue2ros" );
---
+++ Source/ue2ros/ue2rosGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ue2rosGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE2ROS_API Aue2rosGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
